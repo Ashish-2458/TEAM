cdce7e965b2e77f0737ff19af8e4322df94924f8
Ashish Maurya <160274733+Ashish-2458@users.noreply.github.com>
2024-10-04 22:51:12 +0000

    trans

--- GoogleTranslate.js
+++ GoogleTranslate.js
@@ -1,21 +1,37 @@
-// src/components/GoogleTranslate.js
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const GoogleTranslate = () => {
-    useEffect(() => {
-        window.googleTranslateElementInit = function () {
-            new window.google.translate.TranslateElement(
-                { pageLanguage: 'en' },
-                'google_translate_element'
-            );
-        };
+  useEffect(() => {
+    const addGoogleTranslateScript = () => {
+      // Create a new script element
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.async = true;
 
-        const script = document.createElement('script');
-        script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-        document.body.appendChild(script);
-    }, []);
+      // Set the source of the script
+      script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
 
-    return <div id="google_translate_element"></div>;
+      // Append the script to the document's body
+      document.body.appendChild(script);
+
+      // Initialize Google Translate
+      window.googleTranslateElementInit = () => {
+        new window.google.translate.TranslateElement(
+          { pageLanguage: "en", layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE },
+          "google_translate_element"
+        );
+      };
+    };
+
+    addGoogleTranslateScript();
+  }, []);
+
+  return (
+    <div>
+      {/* The div where the Google Translate widget will be rendered */}
+      <div id="google_translate_element"></div>
+    </div>
+  );
 };
 
 export default GoogleTranslate;
