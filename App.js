31a9f11514a51c6fa5f2c3615f0d789edf886d2e
Sanjeev Desai <sanjudesai1806@gmail.com>
2024-10-04 05:13:39 +0000

    footer

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Educational Website</h1>
+        <p>Learn and grow with us!</p>
+      </header>
+      {/* Render Footer Component */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
